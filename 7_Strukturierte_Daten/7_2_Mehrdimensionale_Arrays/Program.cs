c11773f2f3a50fc95ae7f690d913c8e9456d951d
Shamil <kurbanov@outlook.de>
2023-08-16 14:57:06 +0200

    Add 7_2_Mehrdimensionale_Arrays, 7_3_Typisierte_Listen, 7_4_Das_Dictionary_die_elegante_Listenverwaltung

---
+++ 7_Strukturierte_Daten/7_2_Mehrdimensionale_Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+//string[,] spielfeld = new string[3, 3];
+
+var spielfeld = new string[4, 4]
+{
+    { "", "A", "B", "C" },
+    { "1", "#", "#", "#" },
+    { "2", "#", "#", "#" },
+    { "3", "#", "#", "#" }
+};
+
+for (int i = 0; i < spielfeld.GetLength(0); i++)
+{
+    for (int j = 0; j < spielfeld.GetLength(1); j++)
+    {
+        Write(spielfeld[i, j]);
+    }
+    WriteLine();
+}
---
+++ 7_Strukturierte_Daten/7_3_Typisierte_Listen/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
+
+List<string> wochentage = new List<string>(); // leere Liste
+
+wochentage.Add("Montag");  // Elemente hinzufügen
+wochentage.Add("Dienstag");
+wochentage.Add("Mittwoch");
+wochentage.Add("Donnerstag");
+wochentage.Add("Freitag");
+wochentage.Add("Samstag");
+wochentage.Add("Sonntag");
+
+WriteLine("Anzahl der Wochenenden: " + wochentage.Count);
+
+WriteLine("Der erste Tah der Woche ist " + wochentage[0]);
+
+WriteLine();
+
+// Elemente ausgeben
+/*
+foreach(var tag in wochentage){
+    WriteLine(tag);
+}
+*/
+
+WriteLine();
+// Elemente ausgeben
+WriteLine(string.Join(", ", wochentage)); // Ausgabe als String
+
+var arbeitsWoche = wochentage.GetRange(0, 5); // Teil-Liste
+WriteLine("Arbeitswoche: " + string.Join(", ", arbeitsWoche));
+
+var vollerWoche = new List<string>();
+vollerWoche.AddRange(wochentage); // Liste kopieren
+
+vollerWoche.Remove("Montag"); // Element entfernen
+WriteLine("Volle Woche ohne Montag: \n" + string.Join(", ", vollerWoche));
+
+WriteLine("Entfernen wir noch Sontag mit RemoveAt(5):");
+vollerWoche.RemoveAt(5); // Element Sontag entfernen
+
+WriteLine("Volle Woche ohne Montag und Sonntag: \n" + 
+          string.Join(", ", vollerWoche));
+
+WriteLine("Entfernen wir noch Dienstag mit RemoveAt(1):");
+vollerWoche.RemoveAt(1); // Element Dienstag entfernen
+
+WriteLine("Volle Woche ohne Montag, Dienstag und Sonntag: \n" + 
+          string.Join(", ", vollerWoche));
+
+
+
+
+
